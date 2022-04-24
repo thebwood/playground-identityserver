f10da8ee877d5c709233d083403c9c0ab8aaec6f
thebwood <brian.d.wood@gmail.com>
2022-04-24 10:34:03 -0400

    Adding initial solution files

---
+++ PlaygroundIdentityServer/Services/People/People.API/Controllers/PeopleController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace People.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController : ControllerBase
+    {
+    }
+}
