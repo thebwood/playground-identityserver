be23a60141fa85fdec081d6a0ecdb225bae4f2c4
thebwood <brian.d.wood@gmail.com>
2022-04-24 20:34:26 -0400

    IdentityServer is now working

--- PlaygroundIdentityServer/Server/IdentityServer.Server/Program.cs
+++ PlaygroundIdentityServer/Server/IdentityServer.Server/Program.cs
@@ -1,3 +1,5 @@
+using IdentityServer4.Models;
+using IdentityServer4.Test;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,16 +8,11 @@ var assembly = typeof(Program).Assembly.GetName().Name;
 // Add services to the container.
 var connectionString = builder.Configuration["Identity"];
 builder.Services.AddIdentityServer()
-    .AddConfigurationStore(options =>
-    {
-        options.ConfigureDbContext = b =>
-        b.UseSqlServer(connectionString, opt => opt.MigrationsAssembly(assembly));
-    })
-    .AddOperationalStore(options =>
-    {
-        options.ConfigureDbContext = b =>
-        b.UseSqlServer(connectionString, opt => opt.MigrationsAssembly(assembly));
-    })
+    .AddInMemoryClients(new List<Client>())
+    .AddInMemoryIdentityResources(new List<IdentityResource>())
+    .AddInMemoryApiResources(new List<ApiResource>())
+    .AddInMemoryApiScopes(new List<ApiScope>())
+    .AddTestUsers(new List<TestUser>())
     .AddDeveloperSigningCredential();
 
 
